34e220dce68e571f867738eb807bb9497afad563
Aftab Mustafa <mustafa.aftab8906@yahoo.com>
2024-03-02 20:48:04 +0530

    refactor select element

--- src/components/InventoryForm/index.jsx
+++ src/components/InventoryForm/index.jsx
@@ -1,4 +1,12 @@
 import { useState } from 'react'
+import Select from 'react-select'
+import {
+  fabricOptions,
+  itemOptions,
+  sizeOptions,
+  colorOptions,
+  qtyOptions,
+} from '../../utils/constants'
 import './index.css'
 
 function InventoryForm() {
@@ -37,93 +45,47 @@ function InventoryForm() {
       <form onSubmit={handleDefault}>
         <fieldset>
           <legend>Inventory Stock Update</legend>
-
-          <select
-            id='fabric'
-            name='fabric_options'
-            defaultValue={fabricOption}
-            onChange={handleFabricChange}
-          >
-            <option value='Fabric' hidden>
-              Fabric
-            </option>
-            <option value='fabirc#1'>Fabric#1</option>
-            <option value='fabirc#2'>Fabric#2</option>
-            <option value='fabirc#3'>Fabric#3</option>
-            <option value='fabirc#4'>Fabric#4</option>
-            <option value='fabirc#5'>Fabric#5</option>
-            <option value='fabirc#6'>Fabric#6</option>
-          </select>
-
-          <select
-            id='item'
-            name='item_options'
-            defaultValue={itemOption}
-            onChange={handleItemChange}
-          >
-            <option value='Item' hidden>
-              Item
-            </option>
-            <option value='item#1'>Item#1</option>
-            <option value='item#2'>Item#2</option>
-            <option value='item#3'>Item#3</option>
-            <option value='item#4'>Item#4</option>
-            <option value='item#5'>Item#5</option>
-            <option value='item#6'>Item#6</option>
-          </select>
-
-          <select
-            id='size'
-            name='size_options'
-            defaultValue={sizeOption}
-            onChange={handleSizeChange}
-          >
-            <option value='' hidden>
-              Size
-            </option>
-            <option value='size#1'>Size#1</option>
-            <option value='size#2'>Size#2</option>
-            <option value='size#3'>Size#3</option>
-            <option value='size#4'>Size#4</option>
-            <option value='size#5'>Size#5</option>
-            <option value='size#6'>Size#6</option>
-          </select>
-
-          <select
-            id='color'
-            name='color_options'
-            defaultValue={colorOption}
-            onChange={handleColorChange}
-          >
-            <option value='' hidden>
-              Color
-            </option>
-            <option value='color#1'>Color#1</option>
-            <option value='color#2'>Color#2</option>
-            <option value='color#3'>Color#3</option>
-            <option value='color#4'>Color#4</option>
-            <option value='color#5'>Color#5</option>
-            <option value='color#6'>Color#6</option>
-          </select>
-
-          <select
-            id='qty'
-            name='qty_options'
-            defaultValue={qtyOption}
-            onChange={hanldeQtyChange}
-          >
-            <option value='' hidden>
-              Qty
-            </option>
-            <option value='qty#1'>Qty#1</option>
-            <option value='qty#2'>Qty#2</option>
-            <option value='qty#3'>Qty#3</option>
-            <option value='qty#4'>Qty#4</option>
-            <option value='qty#5'>Qty#5</option>
-            <option value='qty#6'>Qty#6</option>
-          </select>
+          <Select
+            value={fabricOption}
+            onChange={setFabricOption}
+            options={fabricOptions}
+            maxMenuHeight={200}
+            placeholder={fabricOption}
+            className='customReactSelect'
+          />
+          <Select
+            value={itemOption}
+            onChange={setItemOption}
+            options={itemOptions}
+            maxMenuHeight={200}
+            placeholder={itemOption}
+            className='customReactSelect'
+          />
+          <Select
+            value={sizeOption}
+            onChange={setSizeOption}
+            options={sizeOptions}
+            maxMenuHeight={200}
+            placeholder={sizeOption}
+            className='customReactSelect'
+          />
+          <Select
+            value={colorOption}
+            onChange={setColorOption}
+            options={colorOptions}
+            maxMenuHeight={200}
+            placeholder={colorOption}
+            className='customReactSelect'
+          />
+          <Select
+            value={qtyOption}
+            onChange={setQtyOption}
+            options={qtyOptions}
+            maxMenuHeight={200}
+            placeholder={qtyOption}
+            className='customReactSelect'
+          />
         </fieldset>
-
         <button>Next</button>
       </form>
     </div>
---
+++ src/utils/constants.js
@@ -0,0 +1,64 @@
+export const fabricOptions = [
+  { value: 'fabric#1', label: 'Fabric#1' },
+  { value: 'fabric#2', label: 'Fabric#2' },
+  { value: 'fabric#3', label: 'Fabric#3' },
+  { value: 'fabric#4', label: 'Fabric#4' },
+  { value: 'fabric#5', label: 'Fabric#5' },
+  { value: 'fabric#6', label: 'Fabric#6' },
+  { value: 'fabric#7', label: 'Fabric#7' },
+  { value: 'fabric#8', label: 'Fabric#8' },
+  { value: 'fabric#9', label: 'Fabric#9' },
+  { value: 'fabric#10', label: 'Fabric#10' },
+]
+
+export const itemOptions = [
+  { value: 'item#1', label: 'Item#1' },
+  { value: 'item#2', label: 'Item#2' },
+  { value: 'item#3', label: 'Item#3' },
+  { value: 'item#4', label: 'Item#4' },
+  { value: 'item#5', label: 'Item#5' },
+  { value: 'item#6', label: 'Item#6' },
+  { value: 'item#7', label: 'Item#7' },
+  { value: 'item#8', label: 'Item#8' },
+  { value: 'item#9', label: 'Item#9' },
+  { value: 'item#10', label: 'Item#10' },
+]
+
+export const sizeOptions = [
+  { value: 'size#1', label: 'Size#1' },
+  { value: 'size#2', label: 'Size#2' },
+  { value: 'size#3', label: 'Size#3' },
+  { value: 'size#4', label: 'Size#4' },
+  { value: 'size#5', label: 'Size#5' },
+  { value: 'size#6', label: 'Size#6' },
+  { value: 'size#7', label: 'Size#7' },
+  { value: 'size#8', label: 'Size#8' },
+  { value: 'size#9', label: 'Size#9' },
+  { value: 'size#10', label: 'Size#10' },
+]
+
+export const colorOptions = [
+  { value: 'color#1', label: 'Color#1' },
+  { value: 'color#2', label: 'Color#2' },
+  { value: 'color#3', label: 'Color#3' },
+  { value: 'color#4', label: 'Color#4' },
+  { value: 'color#5', label: 'Color#5' },
+  { value: 'color#6', label: 'Color#6' },
+  { value: 'color#7', label: 'Color#7' },
+  { value: 'color#8', label: 'Color#8' },
+  { value: 'color#9', label: 'Color#9' },
+  { value: 'color#10', label: 'Color#10' },
+]
+
+export const qtyOptions = [
+  { value: 'qty#1', label: 'Qty#1' },
+  { value: 'qty#2', label: 'Qty#2' },
+  { value: 'qty#3', label: 'Qty#3' },
+  { value: 'qty#4', label: 'Qty#4' },
+  { value: 'qty#5', label: 'Qty#5' },
+  { value: 'qty#6', label: 'Qty#6' },
+  { value: 'qty#7', label: 'Qty#7' },
+  { value: 'qty#8', label: 'Qty#8' },
+  { value: 'qty#9', label: 'Qty#9' },
+  { value: 'qty#10', label: 'Qty#10' },
+]
